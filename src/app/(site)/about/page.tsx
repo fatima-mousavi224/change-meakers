d00bc4b95c9170f7e7739396912528d248a4dee7
Fatima Mousavi <fatimamousavi224@gmail.com>
2026-08-03 11:02:41 +0430

    delate RecognitionExperience component

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/about-us/hero";
 // import Primary from "@/components/about-us/primary";
-import RecognitionExperience from "@/components/about-us/RecognitionExperience";
 import Team from "@/components/about-us/team";
 import SiteContainer from "@/components/common/SiteContainer";
 import React from "react";
@@ -9,7 +8,6 @@ export default function Page() {
   return (
     <SiteContainer className="space-y-12 sm:space-y-16 lg:space-y-20">
       <About />
-      <RecognitionExperience />
       {/* <Primary /> */}
       <Team />
     </SiteContainer>
